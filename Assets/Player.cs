33c1e6482028bd58894263d9e9a0fbe082a2c329
Denis <57326202+Kogwork@users.noreply.github.com>
2021-11-12 02:37:24 +0200

    Tried to animate player(sorry animators)

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,15 +6,29 @@ public class Player : MonoBehaviour
 {
     [SerializeField] public int speed = 5;
     [SerializeField] public Rigidbody2D body;
+    public Animator anim;
 
     // Update is called once per frame
 
     Vector2 movement;
 
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        if(Input.GetAxisRaw("Vertical") != 0f || Input.GetAxisRaw("Horizontal") != 0f) 
+        {
+            anim.SetBool("walk",true);
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
     }
 
     private void FixedUpdate()
